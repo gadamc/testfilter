d3162ed223e0a1c9a1b867c7eafb72c6c16167b9
Adam Cox <adam.cox@kit.edu>
2011-11-21 14:13:02 +0100

    first update of testTrapKamperProto

---
+++ testTrapKamperProto.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+
+from ROOT import *
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+
+global figObj
+
+global trapKamp = KTrapKamperProto
+
+#
+def changeTrapParameters(trap, message = None):
+  d_orig_t = trap.GetDecayTimeConstant()
+  ri_orig_t = trap.GetRiseTime()
+  ff_orig_t = trap.GetFlatTopWidth()
+  
+  if message:
+    print message
+    
+  vals = raw_input('Set trap parameters (decay %f, rise %d, flat %d): ' % (d_orig_t, ri_orig_t, ff_orig_t)) 
+  if vals != '':
+    d = float(vals.split(' ')[0])
+    ri = int(vals.split(' ')[1])
+    ff = int(vals.split(' ')[2])
+    print str(d), ri, ff
+    trap.SetParams(d,ri,ff)
+    # return (d, ri, ff)
+    #   else:
+    #     return (d_orig_t, ri_orig_t, ff_orig_t)
+
+
+
+#
+def changeTrapKamperPeakFinderAmp(trapKamp):
+      
+      
+      
+#
+def plotPulseInfo(p, r):
+
+  global trapKamp
+  
+  print p.GetChannelName()
+  polCalc = KPulsePolarityCalculator()
+  print 'bias', p.GetPolarity()
+  print 'polarity', polCalc.GetExpectedPolarity(p)
+  
+  pulse = np.array(p.GetTrace())
+  
+  plt.subplot(5,1,1)
+  plt.cla()
+  plt.plot(pulse)
+  plt.title('raw')
+  
+  if p.GetIsHeatPulse():
+    trapAmp = trapKamp.GetTrapHeatAmplitude()
+  else:
+    trapAmp = trapKamp.GetTrapIonAmplitude()
+    
+  cleanPulse = np.array([])
+  for i in range(trapAmp.GetInputPulseSize()):
+    cleanPulse = np.append(cleanPulse, trapAmp.GetInputPulse()[i])
+  
+  plt.subplot(5,1,2)
+  plt.cla()
+  plt.plot(cleanPulse)
+  plt.title('cleaned')
+  
+  peakPosResult = trapKamp.GetPeakPositionResult()
+  
+  plt.subplot(5,1,3)
+  plt.cla()
+  plt.plot(np.array(peakPosResult))
+  plt.title('peakPos')
+  
+  numNonZero = 0
+  for val in peakPosResult:
+    if val != 0:
+      numNonZero += 1
+  print 'num peaks', numNonZero
+  
+  
+  ampPulse = np.array([])
+  for i in range(trapAmp.GetOutputPulseSize()):
+    ampPulse = np.append(ampPulse, trapAmp.GetOutputPulse()[i])
+      
+  plt.subplot(5,1,4)
+  plt.cla()
+  plt.plot(ampPulse)
+  plt.title('amplitude trapezoid')
+  
+  print 'results:'
+  print 'amp / peak position', r.GetAmp, '/', r.GetPeakPosition()
+#
+def analyzePulse(p):
+
+  global trapKamp
+  r = KPulseAnalysisRecord()
+  trapKamp.MakeKamp(p,r)
+  plotPulseInfo(p,r)
+  
+  go = raw_input('Change Trap Parameters?(y/n/s/quit)')
+  while(go == 'y'):
+    
+    if p.GetIsHeatPulse():
+      heat_decay_fast, heat_rt_fast, heat_ft_fast = changeTrapParameters(trapFast, 'fast trap' )
+      heat_decay_slow, heat_rt_slow, heat_ft_slow = changeTrapParameters(trapSlow, 'slow trap')
+    else:
+      ion_decay_fast, ion_rt_fast, ion_ft_fast = changeTrapParameters(trapFast, 'fast trap')
+      ion_decay_slow, ion_rt_slow, ion_ft_slow = changeTrapParameters(trapSlow, 'slow trap')
+      
+    trapSlow.SetInputPulse(ptaToTraps.GetOutputPulse(), ptaToTraps.GetOutputPulseSize())
+    trapSlow.RunProcess()
+    trapFast.SetInputPulse(ptaToTraps.GetOutputPulse(), ptaToTraps.GetOutputPulseSize())
+    trapFast.RunProcess()
+    
+    #plot the results
+    plotTheTraps(trapFast, 'fast trap', trapSlow, 'slow trap')
+    
+    go = raw_input('Change Trap Parameters?(y/n/s/quit)')
+    
+  return go
+#
+def runApp(*argv):
+  global figObj
+ 
+ 
+  f = KDataReader(argv[0])
+  e = f.GetEvent()
+  figObj = plt.figure(1)
+  plt.ion()
+  
+  for entry in range(f.GetEntries()):
+    f.GetEntry(entry)
+
+    for j in range(e.GetNumBolos()):
+      b = e.GetBolo(j)
+            
+      for k in range(b.GetNumPulseRecords()):
+        p = b.GetPulseRecord(k)
+        
+        print p.GetChannelName()
+        answer = plotPulse(p)
+        if answer == 'quit':
+          sys.exit(0)
+        if answer == 's':
+          break  #skip to the next bolometer 
+
+       
+if __name__ == '__main__':
+  runApp(*sys.argv[1:])
