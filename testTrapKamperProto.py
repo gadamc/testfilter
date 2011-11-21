c30d49478da70555ab4fa3b2f49a4f0d247b1169
Adam Cox <adam.cox@kit.edu>
2011-11-21 14:35:14 +0100

    udpate

--- testTrapKamperProto.py
+++ testTrapKamperProto.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 global figObj
-global trapKamp = KTrapKamperProto
+global trapKamp = KTrapKamperProto()
 #
 def getAmplitudeTrap(p):
   global trapKamp
