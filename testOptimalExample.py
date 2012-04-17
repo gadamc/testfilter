8e82ff25f7c8afc3965594ee6da6b23bd30722e5
Adam Cox <gadamc@gmail.com>
2012-04-17 14:45:48 +0200

    fix

--- testOptimalExample.py
+++ testOptimalExample.py
@@ -35,6 +35,7 @@ template_doc = vr.first()['doc']
 #have to 'massage' the template a bit here. the template parameters are stored in units of ms. 
 #so make sure to convert properly between time and digitized sample number.
 
+print 'grabbing template from database'
 vp = std.vector("double")()
 exec( template_doc['formula']['rise_double_decay']['python'])  #defines 'template' function used in the following for-loop
 for i in range( 512 ):
@@ -44,6 +45,7 @@ scaleFactor = 1./abs(min(np.array(vp)))
 for i in range(vp.size()):
   vp[i] = scaleFactor * vp[i]
    
+print 'creating pulse processors for analysis'
 #create the pulse processors that are needed for this analysis
 bas = KBaselineRemoval() 
 r2hc = KRealToHalfComplexDFT()
@@ -53,7 +55,7 @@ optFilter = KOptimalFilter()
 
 #configure the ERA peak finder
 era.SetOrder(5)
-era.SetNumRuns(2.3)
+era.SetNumRms(2.3)
 era.SetPolarity(-1)  #tell the ERA peak finder to only look for negative polarity 
 
 #put the pulse template fourier transform into the optimal filter.
@@ -71,11 +73,13 @@ results = dict()
 results['amp'] = []
 results['time'] = []
 
+print 'starting loop over data files'
 for row in viewResults: 
   
   if row['key'] != 'ma22a000': continue #skip all runs but ma22a000
   doc = db[row['id']] #grab the document from the database.
-      
+  
+  print 'searching for noise events in', doc['file']
   #open the file
   f = KDataReader( doc['file'] )
   e = f.GetEvent()  #get the KRawEvent object
@@ -89,34 +93,34 @@ for row in viewResults:
   for i in range(f.GetEntries()):
     f.GetEntry(i)
   
-  for ii in range(e.GetNumBolosPulses()):
-    p = e.GetBoloPulse(ii)  #returns a KRawBoloPulseRecord
+    for ii in range(e.GetNumBolosPulses()):
+      p = e.GetBoloPulse(ii)  #returns a KRawBoloPulseRecord
     
-    if p.GetPulseLength() == 0: continue  #skip empty pulses
+      if p.GetPulseLength() == 0: continue  #skip empty pulses
       
-    if p.GetChannelName() == myChannel:        
+      if p.GetChannelName() == myChannel:        
             
-      bas.SetInputPulse( p.GetTrace()  )
-      bas.RunProcess()    
-      era.SetInputPulse( bas.GetOutputPulse(), bas.GetOuputPulseSize() )
-      era.RunProcess()
-      if era.GetPeakBins().size() > 0:  continue  #skip pulse if we didn't find a noise pulse
+        bas.SetInputPulse( p.GetTrace()  )
+        bas.RunProcess()    
+        era.SetInputPulse( bas.GetOutputPulse(), bas.GetOuputPulseSize() )
+        era.RunProcess()
+        if era.GetPeakBins().size() > 0:  continue  #skip pulse if we didn't find a noise pulse
         
-      r2hc.SetInputPulse( bas.GetOutputPulse(), bas.GetOutputPulseSize() )
-      r2hc.RunProcess()
-      hc2p.SetInputPulse ( r2hc.GetOutputPulse(), r2hc.GetOutputPulseSize() )
-      hc2p.RunProcess()
+        r2hc.SetInputPulse( bas.GetOutputPulse(), bas.GetOutputPulseSize() )
+        r2hc.RunProcess()
+        hc2p.SetInputPulse ( r2hc.GetOutputPulse(), r2hc.GetOutputPulseSize() )
+        hc2p.RunProcess()
       
-      #i assume here that the pulse length never changes within a run
-      if numOfSpectra == 0:
-        power.resize(hc2p.GetOutputPulseSize())
-        for  k in range(power.size()):
-          power[k] = hc2p.GetOutputPulse()[k]
-      else:
-        for k in range(power.size()):
-          power[k] = power[k] * (numOfSpectra-1.0)/numOfSpectra + hc2p.GetOutputPulse()[k] / numOfSpectra
+        #i assume here that the pulse length never changes within a run
+        if numOfSpectra == 0:
+          power.resize(hc2p.GetOutputPulseSize())
+          for  k in range(power.size()):
+            power[k] = hc2p.GetOutputPulse()[k]
+        else:
+          for k in range(power.size()):
+            power[k] = power[k] * (numOfSpectra-1.0)/numOfSpectra + hc2p.GetOutputPulse()[k] / numOfSpectra
       
-      numOfSpectra += 1.0  
+        numOfSpectra += 1.0  
   
   #done looping through the data, now set the power spectrum in the optimal filter      
   optFilter.SetNoiseSpectrum(power)
@@ -130,27 +134,27 @@ for row in viewResults:
   for i in range(f.GetEntries()):
     f.GetEntry(i)
   
-  for ii in range(e.GetNumBolosPulses()):
-    p = e.GetBoloPulse(ii)  #returns a KRawBoloPulseRecord
+    for ii in range(e.GetNumBolosPulses()):
+      p = e.GetBoloPulse(ii)  #returns a KRawBoloPulseRecord
     
-    if p.GetPulseLength() == 0: continue  #skip empty pulses
+      if p.GetPulseLength() == 0: continue  #skip empty pulses
     
-    if p.GetChannelName() == myChannel:    
+      if p.GetChannelName() == myChannel:    
         
-      bas.SetInputPulse( p.GetTrace() )
-      bas.RunProcess()
-      optFilter.SetInputPulse( bas.GetOutputPulse(), bas.GetOutputPulseSize() )
-      optFilter.RunProcess()
+        bas.SetInputPulse( p.GetTrace() )
+        bas.RunProcess()
+        optFilter.SetInputPulse( bas.GetOutputPulse(), bas.GetOutputPulseSize() )
+        optFilter.RunProcess()
       
-      amp = get_out( optFilter )
-      subamp = amp[:20]  #only look at the first 20 bins of the output amplitude estimator
+        amp = get_out( optFilter )
+        subamp = amp[:20]  #only look at the first 20 bins of the output amplitude estimator
       
-      results['amp'].append( subamp[subamp.argmax()] )
-      results['peaktime'].append( subamp.argmax() )
+        results['amp'].append( subamp[subamp.argmax()] )
+        results['peaktime'].append( subamp.argmax() )
       
     
-hall = TH1D('hall', 'hall', 15000, -100, 14000)      
-hpeak =   TH1D('hpeak', 'hpeak', 15000, -100, 14000)
+hall = TH1D('hall', 'hall', 15000, -1000, 14000)      
+hpeak =   TH1D('hpeak', 'hpeak', 15000, -1000, 14000)
   
 for i in range( len(results['amp']) ):
   hall.Fill( results['amp'][i] )
